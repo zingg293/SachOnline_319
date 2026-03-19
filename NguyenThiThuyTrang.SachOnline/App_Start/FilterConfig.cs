592c264bf9c3a0040f2472c90f09aefa9f931dbf
zingg293 <ntttrang29302@gmail.com>
2026-03-19 18:47:49 +0700

    update lab 1

---
+++ NguyenThiThuyTrang.SachOnline/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace NguyenThiThuyTrang.SachOnline
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ NguyenThiThuyTrang.SachOnline/Controllers/SachOnlineController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NguyenThiThuyTrang.SachOnline.Controllers
+{
+    public class SachOnlineController : Controller
+    {
+        // GET: SachOnline
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Partial
+        public ActionResult NavPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult SliderPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult ChuDePartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult NhaXuatBanPartial()
+        {
+            return PartialView(); 
+        }
+
+        public ActionResult SachBanNhieuPartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult FooterPartial()
+        {
+            return PartialView();
+        }
+    }
+}
